3ac5e1d72c80b4080853dde08d2e9f51a9eca904
JuanSBdev <juansb.dev@gmail.com>
2022-05-27 21:49:04 -0300

    seg

---
+++ components/DirectiveHtml.js
@@ -0,0 +1,13 @@
+let directiveHtml = {
+    
+        template:`<div>
+        <h3 v-text="title"> </h3>
+        </div>`,
+        data(){
+            return{
+                title: 'directivas con vue2',
+                message:'Hola desde directives HTML'
+            }
+                
+        }
+    }
--- components/vue-d.js
+++ components/vue-d.js
@@ -1,17 +1,22 @@
 
 Vue.component ('vued', {
     template:`<div>
-    <h1> {{ title }} </h1>
+    <h1 v-text="title"> </h1>
     <h3> asd </h3>
     <a :href="link.href">A</a>
+    <d-html></d-html>
     </div>`,
     data(){
         return{
+            title: 'directivas con vue',
             link: {
                 href: "https://nfybamnh.lucusvirtual.es/",
-                title: 'asdasd'
+                title: 'Directiuv'
             }
         }
             
+    },
+    components: {
+        'd-html': directiveHtml
     }
 })
