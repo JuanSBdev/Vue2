51fece0de61204f4feb12373cda0c0ab25c6a302
JuanSBdev <juansb.dev@gmail.com>
2022-05-27 21:56:21 -0300

    seg

--- components/DirectiveHtml.js
+++ components/DirectiveHtml.js
@@ -2,11 +2,13 @@ let directiveHtml = {
     
         template:`<div>
         <h3 v-text="title"> </h3>
+        <h3 v-html="message"> </h3>
         </div>`,
         data(){
             return{
                 title: 'directivas con vue2',
-                message:'Hola desde directives HTML'
+                message:'<b>Hola desde directives HTML</b>'
+
             }
                 
         }
